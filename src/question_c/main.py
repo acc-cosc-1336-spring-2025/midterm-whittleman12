0de40136d85f5378daadbf379886dd34542ece5b
whittleman12 <whitman.howard@g.austincc.edu>
2025-04-07 04:49:53 +0000

    Coding Exam 1

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,26 @@
 #add import
+import question_c
+
+def random_guesser():
+    choice = "Y"
+    while (choice == "Y" or choice == "y"):
+        value = question_c.get_random_number(1, 5)
+        guess = int(input("Guess a number between 1 and 5: "))
+        if guess == value:
+            print("You win!")
+            choice = input("Press 'Y' to play again: ")
+            if choice == "Y" or "y":
+                continue
+            else:
+                print("Thanks for playing.")
+                break
+        elif guess != value:
+            choice = input("Try again? (Y): ")
+            if choice == "Y" or "y":
+                
+                continue
+            else:
+                print("Thanks for playing.")
+                break
+
+random_guesser()
--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -8,26 +8,3 @@ def test_config():
 def get_random_number(min, max):
     number = random.randint(min, max)
     return number
-
-def random_guesser():
-    choice = "Y"
-    while (choice == "Y" or choice == "y"):
-        value = get_random_number(1, 5)
-        guess = int(input("Guess a number between 1 and 5: "))
-        if guess == value:
-            print("You win!")
-            choice = input("Press 'Y' to play again: ")
-            if choice == "Y" or "y":
-                continue
-            else:
-                print("Thanks for playing.")
-                break
-        elif guess != value:
-            input("Try again? (Y): ")
-            if choice == ("Y" or "y"):
-                continue
-            else:
-                print("Thanks for playing.")
-                break
-
-random_guesser()
